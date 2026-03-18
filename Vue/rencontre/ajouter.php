6a7ddae73f203a4fdb73dfa62e8a73e264f5b7b2
lucasfernandes31 <lucasfernandes82@outlook.fr>
2026-03-18 09:38:00 +0100

    Ajouter Rencontre

--- Vue/rencontre/ajouter.php
+++ Vue/rencontre/ajouter.php
@@ -2,9 +2,11 @@
 
 <?php
 
-use R301\Controleur\RencontreControleur;
 use R301\Modele\Rencontre\RencontreLieu;
 use R301\Vue\Component\Formulaire;
+
+$urlAPI = "http://localhost:8081/rencontre";
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'
         && isset($_POST['dateHeure'])
@@ -12,19 +14,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'
         && isset($_POST['adresse'])
         && isset($_POST['lieu'])
 ) {
-    $controleur = RencontreControleur::getInstance();
+    // On crée d'abord le contenu JSON
+    $data=json_encode([
+        'dateHeure'=>$_POST['dateHeure'],
+        'equipeAdverse'=>$_POST['equipeAdverse'],
+        'adresse'=>$_POST['adresse'],
+        'lieu'=>$_POST['lieu']]);
 
-    if (
-        $controleur->ajouterRencontre(
-            new DateTime($_POST['dateHeure']),
-            $_POST['equipeAdverse'],
-            $_POST['adresse'],
-            RencontreLieu::fromName($_POST['lieu'])
-        )
-    ) {
+    // Ensuite on crée le "stream context", autrement dit les paramètres de la requête et son contenu
+    $context=stream_context_create([
+        'http'=>[
+            'method'=>'POST',
+            'header'=>'Content-Type: application/json',
+            'content'=>$data,
+            'ignore_errors'=>true//pour que ça plante pas automatiquement
+        ]
+    ]);
+
+    $response= file_get_contents($urlAPI,false,$context);
+    var_dump($response);
+      var_dump(DateTime::createFromFormat('Y-m-d\TH:i', '2026-03-26T14:00'));
+
+    $responseTab=json_decode($response,true);
+
+    if($responseTab['status_code']==201){
         header('Location: /rencontre');
     }else{
-        error_log("Erreur lors de la création de la rencontre");
+        echo($responseTab['status_message']);
+        error_log("Erreur lors de la création du rencontre");
     }
 } else {
     $formulaire = new Formulaire("/rencontre/ajouter");
